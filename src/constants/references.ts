3510da5cd229bc5a7ba0bd466d78d01c09887a79
swaztica99 <swaztica99@gmail.com>
2026-05-09 14:27:38 +0700

    UPDATE 09/05/2026 AT 14.27PM

--- src/constants/references.ts
+++ src/constants/references.ts
@@ -1,30 +1,9 @@
 export const REFERENCES = [
     {
         id: 1,
-        title: "E-Commerce Furniture",
-        category: "Online Store",
-        image: "https://images.unsplash.com/photo-1524758631624-e2822e304c36?q=80&w=800",
-        url: "https://example.com/furniture", // Ganti dengan link web asli/demo
-    },
-    {
-        id: 2,
-        title: "SaaS Dashboard Landing",
-        category: "Tech Startup",
-        image: "https://images.unsplash.com/photo-1460925895917-afdab827c52f?q=80&w=800",
-        url: "https://example.com/saas",
-    },
-    {
-        id: 3,
         title: "Modern Coffee Shop",
         category: "Landing Page",
         image: "https://images.unsplash.com/photo-1509042239860-f550ce710b93?q=80&w=800",
-        url: "https://kopi-kita-project-1.vercel.app/",
-    },
-    {
-        id: 4,
-        title: "Luxury Real Estate",
-        category: "Company Profile",
-        image: "https://images.unsplash.com/photo-1560518883-ce09059eeffa?q=80&w=800",
-        url: "https://example.com/property",
+        url: "https://coffee-website-1-wine.vercel.app/",
     }
 ];
