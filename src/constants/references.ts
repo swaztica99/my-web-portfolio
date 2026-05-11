a31552e739f0c963d397d74c72d1a24d87c06bf1
swaztica99 <swaztica99@gmail.com>
2026-05-11 10:22:52 +0700

    UPDATE 11/05/2026 AT 10.22AM

--- src/constants/references.ts
+++ src/constants/references.ts
@@ -1,9 +1,16 @@
 export const REFERENCES = [
     {
         id: 1,
-        title: "Modern Coffee Shop",
+        title: "References 1",
         category: "Landing Page",
-        image: "https://images.unsplash.com/photo-1509042239860-f550ce710b93?q=80&w=800",
+        image: "/webpage2.png",
+        url: "https://kopi-kita-project-1.vercel.app/",
+    },
+    {
+        id: 2,
+        title: "References 2",
+        category: "Landing Page",
+        image: "/webpage3.png",
         url: "https://coffee-website-1-wine.vercel.app/",
     }
 ];
